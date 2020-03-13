6f19f40b9a8b6167b23acab834d93b1c63c6ed2f
AnastasiyaPozyomina <pozyomina@gmail.com>
2020-03-13 15:00:54 +0700

    finish adaptiv layout

--- js.js
+++ js.js
@@ -1,12 +1,17 @@
+"use strict";
+
+let closeBtn = document.querySelector('.closebtn').addEventListener('click', closeNav);
+let openBtn = document.querySelector('.open-menu').addEventListener('click', openNav);
+
+
 function openNav() {
   document.getElementById("myNav").style.width = "100%";
 }
 
+
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
-
-
 
 
 
